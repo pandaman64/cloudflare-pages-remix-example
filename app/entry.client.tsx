998252df5b700ef543c6a6a3c15f2251f2ee1b5f
pandaman64 <kointosudesuyo@infoseek.jp>
2023-07-16 15:58:20 +0900

    Use Joy UI

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -4,15 +4,50 @@
  * For more information, see https://remix.run/file-conventions/entry.client
  */
 
+import { CacheProvider } from "@emotion/react";
 import { RemixBrowser } from "@remix-run/react";
-import { startTransition, StrictMode } from "react";
+import {
+  ReactNode,
+  startTransition,
+  StrictMode,
+  useMemo,
+  useState,
+} from "react";
 import { hydrateRoot } from "react-dom/client";
+import ClientStyleContext from "./src/ClientStyleContext";
+import createEmotionCache from "./src/createEmotionCache";
+import { CssBaseline, CssVarsProvider, ThemeProvider } from "@mui/joy";
+
+// https://github.com/mui/material-ui/blob/e6b4c22aac2e78cd5d4009d46ac3b0ec4b6cc26b/examples/material-remix-ts/app/entry.client.tsx
+function ClientCacheProvider({ children }: { children: ReactNode }) {
+  const [cache, setCache] = useState(createEmotionCache());
+
+  const clientStyleContextValue = useMemo(
+    () => ({
+      reset() {
+        setCache(createEmotionCache());
+      },
+    }),
+    []
+  );
+
+  return (
+    <ClientStyleContext.Provider value={clientStyleContextValue}>
+      <CacheProvider value={cache}>{children}</CacheProvider>
+    </ClientStyleContext.Provider>
+  );
+}
 
 startTransition(() => {
   hydrateRoot(
     document,
     <StrictMode>
-      <RemixBrowser />
+      <ClientCacheProvider>
+        <CssVarsProvider>
+          <CssBaseline />
+          <RemixBrowser />
+        </CssVarsProvider>
+      </ClientCacheProvider>
     </StrictMode>
   );
 });
