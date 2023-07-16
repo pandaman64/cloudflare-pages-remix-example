42ae124789e9dfa00408351301c8389427dfbe08
pandaman64 <kointosudesuyo@infoseek.jp>
2023-07-16 16:29:56 +0900

    First loader

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
-import type { V2_MetaFunction } from "@remix-run/cloudflare";
+import { json, type V2_MetaFunction } from "@remix-run/cloudflare";
+import { useLoaderData } from "@remix-run/react";
 
 export const meta: V2_MetaFunction = () => {
   return [
@@ -7,10 +8,15 @@ export const meta: V2_MetaFunction = () => {
   ];
 };
 
+export async function loader() {
+  return json({ hello: "world" });
+}
+
 export default function Index() {
+  const data = useLoaderData<typeof loader>();
   return (
     <main>
-      <h1>Hello, World!</h1>
+      <h1>Hello, {data.hello}!</h1>
     </main>
   );
 }
